52dc90cfe2d24d6ceb1dd8e9ce0300a29a119318
Nicolas Combe <combe98@gmail.com>
2021-09-18 23:17:23 -0500

    Add reverse_sync.sh

---
+++ reverse_sync.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+sourcePaths=(
+  "zshrc"
+  "tmux.conf"
+  "git/gitconfig"
+  "git/gitconfig-dev"
+  "git/gitconfig-work"
+  "config/starship.toml"
+  "config/brewfile/Brewfile"
+  "config/alacritty/alacritty.yml"
+)
+
+destinationPaths=(
+  "${HOME}/.zshrc"
+  "${HOME}/.tmux.conf"
+  "${HOME}/.gitconfig"
+  "${HOME}/.gitconfig-dev"
+  "${HOME}/.gitconfig-work"
+  "${HOME}/.config/starship.toml"
+  "${HOME}/.config/brewfile/Brewfile"
+  "${HOME}/.config/alacritty/alacritty.yml"
+)
+
+for ((idx=0; idx < ${#sourcePaths[@]}; ++idx)); do
+  sp="${sourcePaths[$idx]}"
+  dp="${destinationPaths[$idx]}"
+
+  cp ${dp} ${sp}
+
+  echo "Updated ${sourcePaths[$idx]} successfully."
+done
