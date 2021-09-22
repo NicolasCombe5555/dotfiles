fdeb9c19825ad015aa02ecf064d5a32d04b55d3d
Nicolas Combe <combe98@gmail.com>
2021-09-22 09:07:00 -0500

    single source of truth

---
+++ files.sh
@@ -0,0 +1,33 @@
+sourcePaths=(
+  "zshrc"
+  "tmux.conf"
+  "git/gitconfig"
+  "git/gitconfig-dev"
+  "git/gitconfig-work"
+  "config/starship.toml"
+  "config/nvim/init.vim"
+  "config/nvim/plugins.vim"
+  "config/nvim/settings.vim"
+  "config/nvim/mappings.vim"
+  "config/nvim/which-key.vim"
+  "config/nvim/start-screen.vim"
+  "config/brewfile/Brewfile"
+  "config/alacritty/alacritty.yml"
+)
+
+destinationPaths=(
+  "${HOME}/.zshrc"
+  "${HOME}/.tmux.conf"
+  "${HOME}/.gitconfig"
+  "${HOME}/.gitconfig-dev"
+  "${HOME}/.gitconfig-work"
+  "${HOME}/.config/starship.toml"
+  "${HOME}/.config/nvim/init.vim"
+  "${HOME}/.config/nvim/plugins.vim"
+  "${HOME}/.config/nvim/settings.vim"
+  "${HOME}/.config/nvim/mappings.vim"
+  "${HOME}/.config/nvim/which-key.vim"
+  "${HOME}/.config/nvim/start-screen.vim"
+  "${HOME}/.config/brewfile/Brewfile"
+  "${HOME}/.config/alacritty/alacritty.yml"
+)
--- sync.sh
+++ sync.sh
@@ -1,38 +1,6 @@
 #!/usr/bin/env bash
 
-sourcePaths=(
-  "zshrc"
-  "tmux.conf"
-  "git/gitconfig"
-  "git/gitconfig-dev"
-  "git/gitconfig-work"
-  "config/starship.toml"
-  "config/nvim/init.vim"
-  "config/nvim/plugins.vim"
-  "config/nvim/settings.vim"
-  "config/nvim/mappings.vim"
-  "config/nvim/which-key.vim"
-  "config/nvim/start-screen.vim"
-  "config/brewfile/Brewfile"
-  "config/alacritty/alacritty.yml"
-)
-
-destinationPaths=(
-  "${HOME}/.zshrc"
-  "${HOME}/.tmux.conf"
-  "${HOME}/.gitconfig"
-  "${HOME}/.gitconfig-dev"
-  "${HOME}/.gitconfig-work"
-  "${HOME}/.config/starship.toml"
-  "${HOME}/.config/nvim/init.vim"
-  "${HOME}/.config/nvim/plugins.vim"
-  "${HOME}/.config/nvim/settings.vim"
-  "${HOME}/.config/nvim/mappings.vim"
-  "${HOME}/.config/nvim/which-key.vim"
-  "${HOME}/.config/nvim/start-screen.vim"
-  "${HOME}/.config/brewfile/Brewfile"
-  "${HOME}/.config/alacritty/alacritty.yml"
-)
+source files.sh
 
 for ((idx=0; idx < ${#sourcePaths[@]}; ++idx)); do
   sp="${sourcePaths[$idx]}"
