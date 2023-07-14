5b8860fe1f3ac3192c42d87cb75cf8d9ec8ff4d1
Nicolas Combe <42418337+NicolasCombe5555@users.noreply.github.com>
2023-07-14 13:12:46 -0400

    Update deleteStaleBranches.sh

--- git/scripts/deleteStaleBranches.sh
+++ git/scripts/deleteStaleBranches.sh
@@ -2,7 +2,7 @@
 
 # Loop through all remote branches
 for branch in $(git branch -r | sed /\*/d); do
-        if [ -z "$(git log -1 --since='5 months ago' -s ${branch})" ]; then
+        if [ -z "$(git log -1 --since='1 year ago' -s ${branch})" ]; then
                 echo -e `git show --format="%ci %cr %an" ${branch} | head -n 1` \\t$branch
                 remote_branch=$(echo ${branch} | sed 's#origin/##' )
                 # To delete the branches uncomment the bellow git delete command
